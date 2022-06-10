d9fdaedaf16dcc82bfa326b59d2bba2bc3cec405
zhark10 <verygoodman18@yandex.ru>
2022-06-11 00:13:11 +0300

    add dotenv

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+     API_URL:  process.env.API_URL
+  }
 }
 
 module.exports = nextConfig
