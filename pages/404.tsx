d9748a02e081b43dc650c57fb853472476ca702d
zhark10 <verygoodman18@yandex.ru>
2022-06-10 23:47:15 +0300

    add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { MainLayout } from '../components/MainLayout'
+
+const Home: NextPage = () => {
+  return (
+    <MainLayout title="Home page" keywords="Error page, example next app, Zhark10">
+      Page not found
+    </MainLayout>
+  )
+}
+
+export default Home
